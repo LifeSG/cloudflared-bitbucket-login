3f0e193faa3bfd2e0ee9ae2e1f22ff54354e2e4c
Keith Tan <keith_xw_tan@tech.gov.sg>
2023-01-12 13:34:11 +0800

    [MISC][KT] Add script execution and setup repo

---
+++ bin.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+const { promisify } = require("util");
+const { exec } = require("child_process");
+const execAsync = promisify(exec);
+
+const execute = async (command, showSuccess) => {
+    // Generate access token
+    const { stderr, stdout } = await execAsync(command);
+    if (stderr) {
+        throw stderr;
+    }
+
+    if (showSuccess && stdout) {
+        console.log(stdout);
+    }
+}
+
+const run = async () => {
+    console.log("Retrieving your Cloudflare access token. Please head to your browser to approve");
+    await execute("cloudflared access login https://bitbucket.ship.gov.sg", true);
+
+    console.log("Applying access token to Bitbucket headers");
+    await execute(`git config --global --replace-all http.https://bitbucket.ship.gov.sg/.extraheader "cf-access-token: $(cloudflared access token -app=https://bitbucket.ship.gov.sg)"`);
+
+    await execute("export cloudflared_access_token=$(cloudflared access token -app=https://bitbucket.ship.gov.sg)");
+    console.log("All good to go!");
+};
+
+run();
